b5d0436dad4bbe3055d5c632fe239d6be7ce7f66
Me <park.pavel04@gmail.com>
2023-11-20 02:02:57 +0600

    style

--- js/home.js
+++ js/home.js
@@ -20,3 +20,22 @@ document.addEventListener("DOMContentLoaded", function () {
         fullscreenModal.style.display = 'none';
     };
 });
+
+let mybutton = document.getElementById("scroll");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 550 || document.documentElement.scrollTop > 550) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function scrollToTop() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
