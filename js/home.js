6ff2714a11310fb16539967264a87b5248514393
Me <park.pavel04@gmail.com>
2023-11-20 05:11:44 +0600

    f

--- js/home.js
+++ js/home.js
@@ -23,7 +23,6 @@ document.addEventListener("DOMContentLoaded", function () {
 
 let mybutton = document.getElementById("scroll");
 
-// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
@@ -34,8 +33,25 @@ function scrollFunction() {
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
 function scrollToTop() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0;
 }
+
+function signinPage(){
+  window.location.href = "signin.html"
+}
+
+function homePage(){
+  window.location.href = "home.html"
+}
+
+function google(){
+  window.location.href = "https://accounts.google.com/InteractiveLogin/identifier?continue=https%3A%2F%2Fwww.google.com%2Fsearch%3Fq%3Dgoogle%2Bfonts%26rlz%3D1C5CHFA_enKZ1042KZ1042%26oq%3Dgoogle%26gs_lcrp%3DEgZjaHJvbWUqBggAEEUYOzIGCAAQRRg7MgYIARBFGDkyBggCEEUYOzIGCAMQRRg7MgYIBBBFGEEyBggFEEUYPDIGCAYQRRg8MgYIBxBFGEHSAQc4MTlqMGo3qAIAsAIA%26sourceid%3Dchrome%26ie%3DUTF-8&ec=GAZAAQ&hl=ru&passive=true&ifkv=AVQVeyy4wEzZfb5UFIzFekmSJvtiJVl7E0qxnpCA5uGpw4jmldd4tccwbS3VGwxZAT68Xbf8CX18fA&theme=glif&flowName=GlifWebSignIn&flowEntry=ServiceLogin"
+}
+
+function microsoft(){
+  window.location.href = "https://login.live.com/login.srf?wa=wsignin1.0&rpsnv=16&ct=1700432616&rver=7.3.6960.0&wp=MBI_SSL&wreply=https%3a%2f%2fwww.microsoft.com%2frpsauth%2fv1%2faccount%2fSignInCallback%3fstate%3deyJSdSI6Imh0dHBzOi8vd3d3Lm1pY3Jvc29mdC5jb20vcnUta3oiLCJMYyI6IjQwOTYiLCJIb3N0Ijoid3d3Lm1pY3Jvc29mdC5jb20ifQ&lc=1033&id=74335&aadredir=0"
+}
+
+var continuee = document.getElementById('continueBtn');
