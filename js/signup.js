fc63c6ed9fc9e3e654cfa1a16f0ddc335da4e2b6
Me <park.pavel04@gmail.com>
2023-11-24 01:40:32 +0600

    pavel

--- js/signup.js
+++ js/signup.js
@@ -22,3 +22,27 @@ function homePage(){
 function loginPage(){
     window.location.href = "signin.html";
 }
+
+
+function validateForm(){
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+    var repeatPassword = document.getElementById('cpassword').value;
+
+
+    if (name === '' || email === '' || password === '' || repeatPassword === '') {
+        alert('All fields must be filled out');
+        return;
+    }
+    else if (password !== repeatPassword) {
+        alert('Passwords do not match');
+        return;
+    }
+
+    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      alert('Invalid email address');
+      return;
+    }
+}
